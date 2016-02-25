64e26d8ac8936f1708aa53672dde03c24c08aeca
Rosemaria Rosier <rosierosier@gmail.com>
2016-02-25 13:18:16 -0500

    updated index, css, and js with basic beginnings

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,60 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var handlebars = require('handlebars');
+
+var url = 'https://github.com/rosierosier?tab=repositories';
+function fetchJSONP(url, callback){
+  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+  var script = document.createElement('script');
+
+  window[callbackName] = function(data){
+    delete window[callbackName];
+    document.body.removeChild(script);
+    callback(data);
+  };
+  script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+  document.body.appendChild(script);
+}
+
+function logData(data){
+  var githubRepo = data.results;
+  console.log(githubRepo);
+  githubRepo.forEach(function(value, index, array){
+    var context = {
+      title: array[index].title,
+    };
+    $('.js-githubRepo').append(template(context));
+    console.log(context);
+  });
+  console.log(data.results[0].title);
+}
+fetchJSONP(url, logData);
+
+function fetchGithub(){
+  var githubUrl = 'https://api.github.com/users/rosierosier';
+  var githubUrlRepo = githubUrl + '/repos';
+
+  $.ajax(githubUrl).done(function(userName){
+    userName.results.forEach(function(user){
+      $('.js-github-user').append('<p>' + user.name + '</p>');
+    });
+  });
+
+  $.ajax(githubUrlRepo).done(function(){
+    
+  })
+}
+
+
+// $('.js-planets-button').click(function(){
+//   fetchPlanets();
+// });
+//
+// function fetchPlanets(){
+//   var planetsUrl = baseUrl + 'planets/';
+//
+//   $.ajax(planetsUrl).done(function(planetsList){
+//     planetsList.results.forEach(function(planet){
+//       $('.js-planets-list').append('<li>' + planet.name + '</li>');
+//     });
+//   });
+// }
